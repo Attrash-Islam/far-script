07e9e78f5a793b6bfc919fd2ac7c65ba33e5afa6
Barak Harizi <barak.h@fiverr.com>
2018-12-11 10:51:21 +0200

    rebase master to branch

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const {argv} = process;
 const getCurrentBranch = () => execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
 
 const branchName = argv[2] || getCurrentBranch();
-execSync(`git checkout master && git pull origin master && git checkout ${branchName} && git rebase master`, { stdio: 'inherit' });
+execSync(`git fetch origin && git rebase origin/master ${branchName}`, { stdio: 'inherit' });
