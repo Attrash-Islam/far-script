e9ecaabda173cbe333b5a50c5d47bdedff3db388
Islam Attrash <isattrash@gmail.com>
2019-01-03 11:29:44 +0200

    Add support for dynamic rebase [master is default]

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 #!/usr/bin/env node
+//@ts-check
 const {execSync} = require('child_process');
+const {createInterface} = require('readline');
 const {argv} = process;
 
 const branchName = argv[2] || '-';
-execSync(`git checkout master && git pull origin master && git checkout ${branchName} && git rebase master`, { stdio: 'inherit' });
+
+function ask(question, callback) {
+    var r = createInterface({
+        input: process.stdin,
+        output: process.stdout});
+    r.question(question, function(answer) {
+        r.close();
+        callback(answer);
+    });
+}
+
+ask('Based on master? [ENTER]: ', function(answer) {
+    const branch = (!answer ? 'master' : answer);
+    execSync(`git checkout master && git pull origin master && git checkout ${branchName} && git rebase ${branch}`, { stdio: 'inherit' });
+});
