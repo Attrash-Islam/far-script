152ee558d884e9896186f0f6bb897dcaedf1bc88
Islam Attrash <isattrash@gmail.com>
2019-01-03 14:07:08 +0200

    Fix basedOnBranch

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const {execSync} = require('child_process');
 const {createInterface} = require('readline');
 const {argv} = process;
 
-const branchName = argv[2] || '-';
-
 function ask(question, callback) {
     var r = createInterface({
         input: process.stdin,
@@ -17,6 +15,6 @@ function ask(question, callback) {
 }
 
 ask('Based on master? [ENTER]: ', function(answer) {
-    const branch = (!answer ? 'master' : answer);
-    execSync(`git checkout master && git pull origin master && git checkout ${branchName} && git rebase ${branch}`, { stdio: 'inherit' });
+    const basedOnBranch = (!answer ? 'master' : answer);
+    execSync(`git checkout ${basedOnBranch} && git pull origin ${basedOnBranch} && git checkout - && git rebase ${basedOnBranch}`, { stdio: 'inherit' });
 });
